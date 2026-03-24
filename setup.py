f7091d0df7a4fd91ee1f5ee2e72ac8f52bee28e5
PSYCHEDELIC EXPRESS <djadhwani.n@gmail.com>
2026-03-24 14:41:24 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Solar Panel Defect Classifier',
+    version= '0.0.0',
+    author= 'dmt',
+    author_email= 'abc@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
